f885bf1368088087dde0c2be3124d11704b2e6a2
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-04-28 08:54:39 -0300

    THIRD MARATONA DISCOVER 2 DAY COMMIT (6) :rocket:

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -12,7 +12,9 @@ module.exports = {
       progress: 0,
       done: 0,
       total: jobs.length
-    }
+    };
+    // total de horas por dia de cada job in progress 
+    let jobTotalHours = 0
 
     const updatedJobs = jobs.map((job) => {
       // .map() pega cada item no estilo for each e retorna um item novo 
@@ -20,16 +22,25 @@ module.exports = {
       const remaining = JobUtils.remainingDays(job)
       const status = remaining <= 0 ? 'done' : 'progress'  //if ternário para retornar o status 
       //somando a quantidade de status 
-      statusCount[status] += 1;
       //recebe o conteúdo de status, compara se é igual done or progress, se for igual ele soma +1
+      statusCount[status] += 1;
+
+      // total de horas por dia de cada job in progress
+      jobTotalHours = status == 'progress' ? jobTotalHours + Number(job["daily-hours"]) : jobTotalHours;
+
+
       return {
         ...job,
         remaining,
         status,
         budget: JobUtils.calculateBudget(job, profile["value-hour"])
-      }
-    })
+      };
+    });
 
-    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount })
+    // qts de horas que quero trabalhar
+    // MENOS quantidade de horas/dia de cada job em progress 
+    const freeHours = profile["hours-per-day"] - jobTotalHours;
+
+    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours })
   }
 }
