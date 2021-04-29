e5d081394df3395bba48fb3d156fa326bac87f8f
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-04-29 15:01:43 -0300

    FOURTH MARATONA DISCOVER 2 DAY - COMMIT (4) :rocket:

--- src/db/init.js
+++ src/db/init.js
@@ -1,70 +1,73 @@
 // criação da tabela do backend 
-const Database = require('config')
+const Database = require('./config')
 
-Database()//abre o banco de dados 
+const initDb = {
+  async init() {
+    const db = await Database() // abre o banco de dados 
 
-Database.exec(`CREATE TABLE profile (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT, 
-  name TEXT,
-  avatar TEXT,
-  monthly_budget INT,
-  days_per_week INT,
-  hours_per_day INT,
-  vacation_per_year INT,
-  value_hour INT,
-)`);
+    await db.exec(`CREATE TABLE profile (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT,
+    avatar TEXT,
+    monthly_budget INT,
+    days_per_week INT,
+    hours_per_day INT,
+    vacation_per_year INT,
+    value_hour INT
+  )`);
 
-Database.exec(`CREATE TABLE jobs(
-  id INTEGER PRIMARY KEY AUTO_INCREMENT, 
-  name TEXT,
-  daily_hours INT,
-  total_hours INT,
-  created_at DATE_TIME, 
-)`)
+    await db.exec(`CREATE TABLE jobs(
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT,
+    daily_hours INT,
+    total_hours INT,
+    created_at DATETIME
+  )`)
 
-Database.run(`INSERT INTO profile (
-  name, 
-  avatar, 
-  monthly_budget, 
-  days_per_week,
-  hours_per_day,
-  vacation_per_year,
-  value_hour,
-  ) VALUES (
-    "Guilherme",
-    "https://github.com/GuilhermeGorges.png",
-    3000,
-    5, 
-    5,
-    4,
-    75
-  );`)
+    await db.run(`INSERT INTO profile (
+    name, 
+    avatar, 
+    monthly_budget, 
+    days_per_week,
+    hours_per_day,
+    vacation_per_year,
+    value_hour
+    ) VALUES (
+      "Guilherme",
+      "https://github.com/GuilhermeGorges.png",
+      3000,
+      5, 
+      5,
+      4,
+      75
+    );`)
 
-Database.run(`INSERT INTO jobs(
-    name,
-    daily_hours,
-    total_hours,
-    created_at,
-  ) VALUES(
-    "Pizzaria Guloso",
-    2,
-    1,
-    1619712960453
-  );`)
+    await db.run(`INSERT INTO jobs(
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES(
+      "Pizzaria Guloso",
+      2,
+      1,
+      1619712960453
+    );`)
 
-Database.run(`INSERT INTO jobs(
-    name,
-    daily_hours,
-    total_hours,
-    created_at,
-  ) VALUES(
-    "Projexo X",
-    3,
-    47,
-    1619712960453
-  );`)
+    await db.run(`INSERT INTO jobs(
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES(
+      "Projexo X",
+      3,
+      47,
+      1619712960453
+    );`)
 
+    await db.close() //fecha o banco de dados 
+  }
+};
 
-Database.close()//fecha o banco de dados 
-
-
+initDb.init();
