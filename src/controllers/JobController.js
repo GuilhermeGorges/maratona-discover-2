c6703b0b66af1b994ecce9836e2704d30b90c8bb
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-05-01 15:38:06 -0300

    FOURTH MARATONA DISCOVER 2 DAY - COMMIT (5) :rocket:]

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -11,7 +11,7 @@ module.exports = {
       name: req.body.name,
       "daily-hours": req.body["daily-hours"],
       "total-hours": req.body["total-hours"],
-      created_at: Date.now() // atribuindo data de hoje
+      created_at: Date.now()
     });
 
     return res.redirect('/')
@@ -33,36 +33,21 @@ module.exports = {
     return res.render("job-edit", { job })
   },
   async update(req, res) {
-    const jobs = await Job.get();
-
     const jobId = req.params.id
-    const job = jobs.find(job => Number(job.id) === Number(jobId))
-    if (!job) {
-      return res.send('JOB NOT FOUND!')
-    }
 
     const updatedJob = {
-      ...job, //quando á mais itens precisamos espalhar o conteúdo
       name: req.body.name,
       "total-hours": req.body["total-hours"],
       "daily-hours": req.body["daily-hours"],
     }
 
-    const newJobs = jobs.map(job => {
-      if (Number(job.id) === Number(jobId)) {
-        job = updatedJob
-      }
-
-      return job
-    })
-
-    Job.update(newJobs)
+    await Job.update(updatedJob, jobId)
 
     res.redirect('/job/' + jobId)
   },
-  delete(req, res) {
+  async delete(req, res) {
     const jobId = req.params.id
-    Job.delete(jobId)
+    await Job.delete(jobId)
     return res.redirect('/')
   }
 };
